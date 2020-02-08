618b81f6e11be42228f9e1b3831a306180f79975
EC2 Default User <ec2-user@ip-172-31-95-128.ec2.internal>
2020-02-08 03:39:48 +0000

    Finished HW2

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,6 +2,7 @@ class MoviesController < ApplicationController
 
   def movie_params
     params.require(:movie).permit(:title, :rating, :description, :release_date)
+    #Rails.logger.info("PARAMS: #{params.inspect}")
   end
 
   def show
@@ -11,7 +12,53 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    @all_ratings = Movie.ratings
+    
+    
+    if params[:ratings].nil? && !session[:ratings].nil?
+      @filters = @all_ratings
+      params[:ratings] = session[:ratings]
+    elsif params[:ratings].nil?
+      @filters = @all_ratings
+    else
+      @filters = params[:ratings]
+    end
+      
+    
+    @filters = params[:ratings].nil? ? @all_ratings : params[:ratings]
+    
+    
+    
+    
+    if (params[:ratings])
+      # Save the ratings from the params to the session
+      session[:ratings] = params[:ratings]
+      @filters = params[:ratings]
+      
+    elsif (session[:ratings])
+      # if the ratings doesn't exist in the params 
+      # but it does in the session, then use the ratings from the session
+      if params[:sort].nil?
+        redirect_to movies_path({:ratings => params[:ratings]})
+      else
+        redirect_to movies_path({:sort => params[:sort], :ratings => session[:ratings]})
+      end
+    end
+    
+    if (params[:sort])
+      # Save the sort settings from the params to the session
+      @renderTitle = params[:sort] == "title" ? true : false
+      @renderDate = params[:sort] == "release_date" ? true : false
+      session[:sort] = params[:sort]
+      @movies = Movie.movies(params[:ratings], params[:sort])
+    elsif (session[:sort])
+      # if the sort settings doesn't exist in the params 
+      # but it does in the session, then use the settings from the session
+      redirect_to movies_path({:sort => session[:sort], :ratings => session[:ratings]}) and return
+    else
+      @movies = Movie.movies(params[:ratings], params[:sort]) #hopefully it still sorts if sort is nil
+    end
+    
   end
 
   def new
@@ -39,7 +86,7 @@ class MoviesController < ApplicationController
     @movie = Movie.find(params[:id])
     @movie.destroy
     flash[:notice] = "Movie '#{@movie.title}' deleted."
-    redirect_to movies_path
+    redirect_to movies_path({:sort => session[:sort], :ratings => session[:ratings]})
   end
 
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,13 @@
 class Movie < ActiveRecord::Base
+    def self.movies(ratings ,sortBy)
+        if ratings.nil?
+            self.order(sortBy)
+        else
+            self.where({:rating => ratings.keys}).order(sortBy)
+        end
+    end
+    
+    def self.ratings
+        self.pluck(:rating).uniq
+    end
 end
